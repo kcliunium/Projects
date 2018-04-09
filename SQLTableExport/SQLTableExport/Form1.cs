f687972bb66cec3393fe5cec584b9910ae96c336
kcliunium <kcliunium@gmail.com>
2018-04-09 18:55:16 +0800

    update sql

--- SQLTableExport/SQLTableExport/Form1.cs
+++ SQLTableExport/SQLTableExport/Form1.cs
@@ -25,7 +25,7 @@ namespace SQLTableExport
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string queryString = "DELETE FROM TableSchema1";
+            string queryString = "DELETE FROM TableSchema";
             DeleteCommand(queryString, GetConnectionString());
         }
 
@@ -51,20 +51,33 @@ namespace SQLTableExport
 
         private void UpdateCommand(SqlConnection connection, DataRow row)
         {
-            this.richTextBox1.AppendText("+++ UpdateCommand: " + row[0].ToString() + ", " + row[2].ToString() + ", " + row[5].ToString() + "." + System.Environment.NewLine);
+            this.richTextBox1.AppendText("* UpdateCommand: " + row[0].ToString() + ", " + row[1].ToString() + ", " + row[2].ToString() + ", " + row[3].ToString() + ", " + row[4].ToString() + ", " + row[5].ToString() + ", " + row[6].ToString() + ", " + row[7].ToString() + "." + System.Environment.NewLine);
             this.richTextBox1.SelectionStart = this.richTextBox1.TextLength;
             this.richTextBox1.ScrollToCaret();
 
-            string queryString = "UPDATE TableSchema SET _DF=@_DF,_DF_Value=@_DF_Value WHERE TableName=@TableName and FieldName=@FieldName";
+            string queryString = "UPDATE TableSchema SET SeqNo=@SeqNo,_Type=@_Type,_Len=@_Len,_PK=@_PK,_Null=@_Null,_DF=@_DF,_DESC=@_DESC,updDate=@updDate WHERE TableName=@TableName and FieldName=@FieldName";
 
             using (SqlCommand command = new SqlCommand(queryString, connection))
                 {
                     try
                     {
+                        command.Parameters.Add("@SeqNo", SqlDbType.Int).Value = row[1].ToString();
                         command.Parameters.Add("@TableName", SqlDbType.VarChar).Value = row[0].ToString();
                         command.Parameters.Add("@FieldName", SqlDbType.VarChar).Value = row[2].ToString();
-                        command.Parameters.Add("@_DF", SqlDbType.Char).Value = "4";
-                        command.Parameters.Add("@_DF_Value", SqlDbType.Char).Value = "369456";
+                        command.Parameters.Add("@_Type", SqlDbType.VarChar).Value = row[3].ToString();
+                        command.Parameters.Add("@_Len", SqlDbType.Int).Value = row[4].ToString();
+                        command.Parameters.Add("@_PK", SqlDbType.Char).Value = 1;
+                        if (row[5].ToString().Equals("True"))
+                        {
+                            command.Parameters.Add("@_Null", SqlDbType.Char).Value = 1;
+                        }
+                        else
+                        {
+                            command.Parameters.Add("@_Null", SqlDbType.Char).Value = 0;
+                        }
+                        command.Parameters.Add("@_DF", SqlDbType.Char).Value = row[6].ToString();
+                        command.Parameters.Add("@_DESC", SqlDbType.VarChar).Value = row[7].ToString();
+                        command.Parameters.Add("@updDate", SqlDbType.DateTime).Value = DateTime.Now.ToString();
 
                         command.ExecuteNonQuery();
                     }
@@ -77,12 +90,12 @@ namespace SQLTableExport
 
         private void InsertCommand(SqlConnection connection, DataRow row)
         {
-            this.richTextBox1.AppendText("+ InsertCommand: " + row[0].ToString() + ", " + row[2].ToString() + ", " + row[5].ToString() + "." + System.Environment.NewLine);
+            this.richTextBox1.AppendText("+ InsertCommand: " + row[0].ToString() + ", " + row[1].ToString() + ", " + row[2].ToString() + ", " + row[3].ToString() + ", " + row[4].ToString() + ", " + row[5].ToString() + ", " + row[6].ToString() + ", " + row[7].ToString() + "." + System.Environment.NewLine);
             this.richTextBox1.SelectionStart = this.richTextBox1.TextLength;
             this.richTextBox1.ScrollToCaret();
 
-            string queryString = "INSERT INTO TableSchema(SeqNo,TableName,FieldName,_Type,_Len,_PK,_Null,_DF,_DF_Value,_DESC,updDate)" +
-                    " VALUES(@SeqNo,@TableName,@FieldName,@_Type,@_Len,@_PK,@_Null,@_DF,@_DF_Value,@_DESC,@updDate)";
+            string queryString = "INSERT INTO TableSchema(SeqNo,TableName,FieldName,_Type,_Len,_PK,_Null,_DF,_DESC,updDate)" +
+                    " VALUES(@SeqNo,@TableName,@FieldName,@_Type,@_Len,@_PK,@_Null,@_DF,@_DESC,@updDate)";
 
             using (SqlCommand command = new SqlCommand(queryString, connection))
             {
@@ -93,7 +106,7 @@ namespace SQLTableExport
                     command.Parameters.Add("@FieldName", SqlDbType.VarChar).Value = row[2].ToString();// "PromoNo";
                     command.Parameters.Add("@_Type", SqlDbType.VarChar).Value = row[3].ToString();// "char";
                     command.Parameters.Add("@_Len", SqlDbType.Int).Value = row[4].ToString();// "101";
-                    command.Parameters.Add("@_PK", SqlDbType.Char).Value = "0";
+                    command.Parameters.Add("@_PK", SqlDbType.Char).Value = 0;
                     if (row[5].ToString().Equals("True"))
                     {
                         command.Parameters.Add("@_Null", SqlDbType.Char).Value = 1;
@@ -102,10 +115,9 @@ namespace SQLTableExport
                     {
                         command.Parameters.Add("@_Null", SqlDbType.Char).Value = 0;
                     }
-                    command.Parameters.Add("@_DF", SqlDbType.Char).Value = "0";
-                    command.Parameters.Add("@_DF_Value", SqlDbType.Char).Value = "0";
-                    command.Parameters.Add("@_DESC", SqlDbType.VarChar).Value = "0";
-                    command.Parameters.Add("@updDate", SqlDbType.DateTime).Value = DateTime.Now.ToString();;
+                    command.Parameters.Add("@_DF", SqlDbType.Char).Value = row[6].ToString();
+                    command.Parameters.Add("@_DESC", SqlDbType.VarChar).Value = row[7].ToString();
+                    command.Parameters.Add("@updDate", SqlDbType.DateTime).Value = DateTime.Now.ToString();
 
                     command.ExecuteNonQuery();
                 }
@@ -118,7 +130,7 @@ namespace SQLTableExport
 
         private Boolean SelectCommand(SqlConnection connection, string table, string field)
         {
-            this.richTextBox1.AppendText("- SqlConnection: table = " + table + ", field = " + field + "." + System.Environment.NewLine);
+            this.richTextBox1.AppendText("@ SqlConnection: table = " + table + ", field = " + field + "." + System.Environment.NewLine);
             this.richTextBox1.SelectionStart = this.richTextBox1.TextLength;
             this.richTextBox1.ScrollToCaret();
 
@@ -154,6 +166,11 @@ namespace SQLTableExport
                     "D.[NAME] AS '資料型別', " +
                     "C.[MAX_LENGTH] AS '長度', " +
                     "C.[IS_NULLABLE] AS '是否允許NULL', " +
+                    "E.[TEXT] AS '預設值', " +
+                    "( SELECT value " +
+                    "FROM fn_listextendedproperty (NULL, 'schema', 'dbo', 'table', B.[NAME], 'column', default) " +
+                    "WHERE name='MS_Description' and objtype='COLUMN' and objname Collate Chinese_Taiwan_Stroke_CI_AS=C.[NAME] " +
+                    ") AS '欄位備註', " + 
                     "B.[CREATE_DATE] AS '建立時間', " +
                     "B.[MODIFY_DATE] AS '修改時間' " +
                     "FROM SYS.SCHEMAS A INNER JOIN SYS.TABLES B " +
@@ -162,6 +179,8 @@ namespace SQLTableExport
                     "ON B.[OBJECT_ID] = C.[OBJECT_ID] " +
                     "INNER JOIN SYS.TYPES D " +
                     "ON C.[SYSTEM_TYPE_ID] = D.[SYSTEM_TYPE_ID] AND C.[USER_TYPE_ID] = D.[USER_TYPE_ID] " +
+                    "LEFT JOIN SYS.SYSCOMMENTS E " +
+                    "ON C.[DEFAULT_OBJECT_ID] = E.[ID] " +
                     "WHERE B.[TYPE] = 'U' " +
                     "ORDER BY A.[NAME], B.[NAME]";
 
@@ -170,6 +189,38 @@ namespace SQLTableExport
                 adapter.Fill(dataset);
                 return dataset;
             }
+        }
+
+        private DataSet SelectPrmaryKeyRaws(SqlConnection connection, DataSet dataset)
+        {
+            string queryString = @"SELECT B.[NAME] AS '資料表名稱', " +
+                    "C.[COLUMN_ID] AS '欄位順序', " +
+                    "C.[NAME] AS '欄位名稱', " +
+                    "D.[NAME] AS '資料型別', " +
+                    "C.[MAX_LENGTH] AS '長度', " +
+                    "C.[IS_NULLABLE] AS '是否允許NULL', " +
+                    "E.[TEXT] AS '預設值', " +
+                    "( SELECT value " +
+                    "FROM fn_listextendedproperty (NULL, 'schema', 'dbo', 'table', B.[NAME], 'column', default) " +
+                    "WHERE name='MS_Description' and objtype='COLUMN' and objname Collate Chinese_Taiwan_Stroke_CI_AS=C.[NAME] " +
+                    ") AS '欄位備註', " + 
+                    "B.[CREATE_DATE] AS '建立時間', " +
+                    "B.[MODIFY_DATE] AS '修改時間' " +
+                    "FROM SYS.SCHEMAS A INNER JOIN SYS.TABLES B " +
+                    "ON A.[SCHEMA_ID] = B.[SCHEMA_ID] " +
+                    "INNER JOIN SYS.COLUMNS C " +
+                    "ON B.[OBJECT_ID] = C.[OBJECT_ID] " +
+                    "INNER JOIN SYS.TYPES D " +
+                    "ON C.[SYSTEM_TYPE_ID] = D.[SYSTEM_TYPE_ID] AND C.[USER_TYPE_ID] = D.[USER_TYPE_ID] " +
+                    "LEFT JOIN SYS.SYSCOMMENTS E " +
+                    "ON C.[DEFAULT_OBJECT_ID] = E.[ID] " +
+                    "WHERE B.[TYPE] = 'U' " +
+                    "ORDER BY A.[NAME], B.[NAME]";
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = new SqlCommand(queryString, connection);
+                adapter.Fill(dataset);
+                return dataset;
         }
 
         private void OpenSqlConnection()
